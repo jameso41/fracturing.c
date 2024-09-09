054598e1d2d0974154a0b3ddd2c4c683118ccd70
jameso41 <ja631708@ucf.edu>
2024-09-09 23:15:46 +0000

    setting_up_the_program

---
+++ assignment_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+// define a value for PI
+#define PI 3.14159
+
+int main(int argc, char**argv){
+    //call required functions
+    calculateDistance();
+    calculatePerimeter();
+    calculateArea();
+    calculateWidth();
+    calculateHeight();
+
+    return 0;
+}
